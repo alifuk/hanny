925840771d88b26a564b2ab0e9173af10e3b903e
alifuk <amdalifuk@gmail.com>
2015-06-16 03:55:31 +0200

    quite works

--- myslider.php
+++ myslider.php
@@ -1,12 +1,15 @@
-<div class="myslider">
-    <div class="arrowsContainer">
-        <div class="vCenter">
-            <div class="arrowLeft"><img src="./img/left.png" class="imgArrowLeft"></div>
-            <div class="arrowRight"><img src="./img/right.png" class="imgArrowRight"></div>
-        </div>
+
+<div class="arrowsContainer">
+    <div class="vCenter">
+        <div class="arrowLeft"><img src="./img/left.png" class="imgArrowLeft"></div>
+        <div class="arrowRight"><img src="./img/right.png" class="imgArrowRight"></div>
     </div>
+</div>
+
+<div class="myslider">
+
     <?php
-    $dir = "./foto/zeny/";
+    $dir = "./foto/muzi/";
     $files = scandir($dir, 1);
 
     $zindex = 10;
@@ -26,10 +29,36 @@
 </div>
 
 
+
+
+
+
 <script>
 
     $(document).ready(function () {
         nastavVysku();
+
+        var moveStep = 800;
+
+        $(".arrowLeft").click(function () {
+            move(moveStep);
+        });
+
+        $(".arrowRight").click(function () {
+            move(-1 * moveStep);
+        });
+
+        $(".fotka").hover(function(){
+            $(this).addClass("fotkaScaleIn");
+        },function(){
+            
+        });
+
+
+
+
+
+
 
     });
 
@@ -51,14 +80,33 @@
         });
 
 
-        console.log(sirka);
-
-
-
         $(".arrowsContainer").width($(window).width());
+        $(".arrowsContainer").height($(".myslider").height());
 
-        $(".arrowLeft").height(50);
 
+    }
+
+    function move(smoveStep) {
+
+        var sirka = 0; //šířka obrázků ve slideru
+        $(".fotka").each(function () {
+            sirka = sirka + $(this).width();
+        });
+
+        var imoveStep = parseInt(smoveStep); //o kolik se máme pohnout
+
+
+        var vlevo = $(".myslider").css("left");
+        var vlevoInt = parseInt(vlevo.substring(0, vlevo.length - 2)) + imoveStep;
+        if (vlevoInt >= 0) {
+            vlevoInt = 0;
+        }
+
+        if (vlevoInt + sirka <= +$(window).width()) {
+            vlevoInt = -sirka + $(window).width();
+        }
+
+        $(".myslider").css("left", (vlevoInt + "px").toString());
     }
 
 </script>
